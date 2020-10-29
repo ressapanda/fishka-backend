5b4e22138fd283cf3f39b4a3d0bb6d1718636518
Grzegorz Jaworski <gostekk94@gmail.com>
2020-10-29 01:57:42 +0100

    Updated Question admin models

--- apps/questions/admin.py
+++ apps/questions/admin.py
@@ -1,18 +1,36 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.utils.translation import ngettext
 
 from apps.questions.models import Question
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'difficulty', 'framework', 'team', 'language']
+    list_filter = ['difficulty', 'framework', 'team', 'language']
+
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         return qs.filter(is_public=True)
 
 
 class QuestionSuggestionAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'author_email', 'created_at']
+    ordering = ['created_at']
+    actions = ['make_published']
+
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         return qs.filter(is_public=False)
+
+    def make_published(self, request, queryset):
+        updated = queryset.update(is_public=True)
+        self.message_user(request, ngettext(
+            '%d question was successfully marked as published.',
+            '%d questions were successfully marked as published.',
+            updated,
+        ) % updated, messages.SUCCESS)
+
+    make_published.short_description = "Mark selected questions as published"
 
 
 class QuestionSuggestion(Question):
